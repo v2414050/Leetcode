932d1c4d87083f3c826e6c512d008db84a814968
Vibhor Shukla <2k24.cs1q.2414050@gmail.com>
2026-08-07 10:30:08 +0530

    Time: 99 ms (78.72%), Space: 9.4 MB (46.6%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,59 +1,19 @@
-#include <stdlib.h>
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int a = 0, b = numsSize - 1;
+    int* ar = (int*)malloc(2 * sizeof(int));
 
-
-struct hash_item {
-    int key;
-    int val;
-    UT_hash_handle hh;
-};
-
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {struct hash_item *hash_table = NULL;
-
-    for (int i = 0; i < numsSize; i++) {
-        int current_num = nums[i];
-        int complement = target - current_num;
-
-        struct hash_item *found_item;
-        HASH_FIND_INT(hash_table, &complement, found_item);
-
-        if (found_item != NULL) {
-            int* result = (int*)malloc(2 * sizeof(int));
-            if (result == NULL) {
-                struct hash_item *current, *tmp;
-                HASH_ITER(hh, hash_table, current, tmp) {
-                    HASH_DEL(hash_table, current);
-                    free(current);
-                }
-                *returnSize =0;
-                return NULL;
+    for (int a = 0; a < numsSize; a++) {
+        for (b=a+1;b<numsSize;b++){
+            if (nums[a] + nums[b] == target) {
+                ar[0] = a;
+                ar[1] = b;
+                *returnSize = 2;
+              return ar;
             }
-            result[0] = found_item->val;
-            result[1] = i;
-            *returnSize = 2;
-
-            struct hash_item *current, *tmp;
-            HASH_ITER(hh, hash_table, current, tmp) {
-                HASH_DEL(hash_table, current);
-                free(current);
-            }
-            return result;
         }
-
-        struct hash_item *new_item = (struct hash_item*)malloc(sizeof(struct hash_item));
-        if(new_item == NULL) {
-            struct hash_item *current, *tmp;
-            HASH_ITER(hh, hash_table, current, tmp) {
-                HASH_DEL(hash_table, current);
-                free(current);
-            }
-            *returnSize = 0;
-            return NULL;
-        }
-        new_item->key = current_num;
-        new_item->val = i;
-        HASH_ADD_INT(hash_table, key, new_item);
     }
-    
+
+    free(ar);
     *returnSize = 0;
     return NULL;
 }
