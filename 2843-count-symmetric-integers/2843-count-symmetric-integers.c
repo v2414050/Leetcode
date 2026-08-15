8744812781607e3923b4c0355b676b871da6cb50
Vibhor Shukla <2k24.cs1q.2414050@gmail.com>
2026-08-15 23:34:29 +0530

    Time: 4 ms (68.36%), Space: 9.7 MB (58.59%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.c
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.c
@@ -2,36 +2,16 @@ int countSymmetricIntegers(int low, int high) {
     int count = 0;
     
     for (int num = low; num <= high; num++) {
-        int temp = num;
-        int len = 0;
-        
-        while (temp > 0) {
-            len++;
-            temp /= 10;
-        }
-        
-        if (len % 2 != 0) {
-            continue;
-        }
-        
-        int half = len / 2;
-        int right = num;
-        int sum1 = 0;
-        int sum2 = 0;
-        
-        for (int i = 0; i < half; i++) {
-            sum2 += right % 10;
-            right /= 10;
-        }
-        
-        int left = right;
-        for (int i = 0; i < half; i++) {
-            sum1 += left % 10;
-            left /= 10;
-        }
-        
-        if (sum1 == sum2) {
-            count++;
+        if (num >= 10 && num <= 99) {
+            if ((num / 10) == (num % 10)) {
+                count++;
+            }
+        } else if (num >= 1000 && num <= 9999) {
+            int left = num / 100;
+            int right = num % 100;
+            if ((left / 10 + left % 10) == (right / 10 + right % 10)) {
+                count++;
+            }
         }
     }
     
