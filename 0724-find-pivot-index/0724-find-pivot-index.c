98682d27d616ee73d728e168023083d17d62ee2d
Vibhor Shukla <2k24.cs1q.2414050@gmail.com>
2026-08-19 15:55:53 +0530

    Time: 0 ms (100%), Space: 10.2 MB (94.68%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.c
+++ 0724-find-pivot-index/0724-find-pivot-index.c
@@ -1,15 +1,16 @@
 int pivotIndex(int* nums, int n) {
-    int p[n],s[n],sum=nums[0];
-     p[0]=0,s[0]=0;
+    int sum=nums[0],left =0 ;
     for (int i = 1;i<n;i++){
-        p[i] = p[i - 1] + nums[i - 1];
         sum+=nums[i];
     }
-    for (int i =0 ; i< n ;i++){
-        s[i]=sum-p[i]-nums[i];
-        if (s[i]==p[i]){
+    // if (sum ==0){
+    //     return 0;
+    // }
+    for (int i = 0; i < n; i++) {
+        if (left == sum - left - nums[i]) {
             return i;
         }
+        left += nums[i];
     }
         return-1;
 }
