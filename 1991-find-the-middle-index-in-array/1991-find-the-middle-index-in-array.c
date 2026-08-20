2a14a414d6929bd1ba3b36d3f872793cadc187cb
Vibhor Shukla <2k24.cs1q.2414050@gmail.com>
2026-08-20 10:16:01 +0530

    Time: 0 ms (100%), Space: 9.1 MB (82.71%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.c
@@ -0,0 +1,14 @@
+int findMiddleIndex(int* nums, int n) {
+    int sum=nums[0],left =0 ;
+    for (int i = 1;i<n;i++){
+        sum+=nums[i];
+    }
+    for (int i = 0; i < n; i++) {
+        if (left == sum - left - nums[i]) {
+            return i;
+        }
+        left += nums[i];
+    }
+        return-1;
+  
+}
