bb4f07b5521ac67e9cb9ada56c18945c3c4e464c
Vibhor Shukla <2k24.cs1q.2414050@gmail.com>
2026-08-20 10:55:46 +0530

    Time: 0 ms (100%), Space: 11.2 MB (38.97%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.c
+++ 0412-fizz-buzz/0412-fizz-buzz.c
@@ -2,20 +2,23 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 char** fizzBuzz(int n, int* returnSize) {
-    char **ans = (char**)malloc(sizeof(char*) * n);
-    for(int i = 1; i <= n; i++) {
-        if(i%15==0) {
-            ans[i - 1] = "FizzBuzz";
-        } else if(i % 3 == 0) {
-            ans[i - 1] = "Fizz";
-        } else if(i % 5 == 0) {
-            ans[i - 1] = "Buzz";
+    *returnSize = n;
+    char** ans = (char**)malloc(sizeof(char*) * n);
+
+    for (int i = 1; i <= n; i++) {
+        // Allocate space for max string size (digits up to 10^4 or "FizzBuzz" + '\0')
+        ans[i - 1] = (char*)malloc(sizeof(char) * 9);
+
+        if (i % 15 == 0) {
+            strcpy(ans[i - 1], "FizzBuzz");
+        } else if (i % 3 == 0) {
+            strcpy(ans[i - 1], "Fizz");
+        } else if (i % 5 == 0) {
+            strcpy(ans[i - 1], "Buzz");
         } else {
-            ans[i - 1] = (char*)malloc(sizeof(char) * 12);
             sprintf(ans[i - 1], "%d", i);
         }
-
     }
-    *returnSize=n;
+
     return ans;
 }
