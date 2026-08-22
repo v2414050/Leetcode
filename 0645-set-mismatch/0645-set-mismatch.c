77e1728b3c5c9fda37ae8da525292b7cae57662a
Vibhor Shukla <2k24.cs1q.2414050@gmail.com>
2026-08-22 16:58:22 +0530

    Time: 0 ms (100%), Space: 10.9 MB (17.45%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.c
@@ -0,0 +1,16 @@
+int* findErrorNums(int* nums, int numsSize, int* returnSize) {
+    *returnSize = 2;
+    int* result = (int*)malloc(2 * sizeof(int));
+    int count[10001] = {0}; 
+    
+    for (int i = 0; i < numsSize; i++) {
+        count[nums[i]]++;
+    }
+    
+    for (int i = 1; i <= numsSize; i++) {
+        if (count[i] == 2) result[0] = i; 
+        if (count[i] == 0) result[1] = i; 
+    }
+    
+    return result;
+}
