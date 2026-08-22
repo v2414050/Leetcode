7f7e954605ac0e826ef53683e0491871869daf6c
Vibhor Shukla <2k24.cs1q.2414050@gmail.com>
2026-08-22 17:22:39 +0530

    Time: 0 ms (100%), Space: 12 MB (50.69%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.c
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.c
@@ -2,16 +2,23 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* smallerNumbersThanCurrent(int* nums, int numsSize, int* returnSize) {
-    int * result = (int*)malloc(sizeof(int) * numsSize);
-    for(int i = 0; i < numsSize; i++) {
-        int count = 0;
-        for(int j = 0; j < numsSize; j++) {
-            if(nums[j] < nums[i]) {
-                count++;
-            }
-        }
-        result[i] = count;
+    int count[101] = {0};
+    int* result = (int*)malloc(sizeof(int) * numsSize);
+    
+    // 1. Count occurrences of each number
+    for (int i = 0; i < numsSize; i++) {
+        count[nums[i]]++;
     }
+    int runningSum = 0;
+    for (int i = 0; i <= 100; i++) {
+        int temp = count[i];
+        count[i] = runningSum;
+        runningSum += temp;
+    }
+    for (int i = 0; i < numsSize; i++) {
+        result[i] = count[nums[i]];
+    }
+    
     *returnSize = numsSize;
     return result;
 }
