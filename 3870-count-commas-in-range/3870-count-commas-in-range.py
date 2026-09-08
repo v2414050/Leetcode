4c3a9c93a33ab7c531362ea3d835efb2378c4c5d
Vibhor Shukla <2k24.cs1q.2414050@gmail.com>
2026-09-08 10:32:25 +0530

    Time: 0 ms (100%), Space: 12.4 MB (32.04%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def countCommas(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n/1000==0:
+            return 0 
+        return n-999
