1f5cea059cf68b5e098a2a5bc49e70cdd9603966
Vibhor Shukla <2k24.cs1q.2414050@gmail.com>
2026-09-14 14:14:19 +0530

    Time: 0 ms (100%), Space: 8.6 MB (54.43%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.c
@@ -0,0 +1,6 @@
+bool isRectangleOverlap(int* rec1, int rec1Size, int* rec2, int rec2Size) {
+     if (rec1[0]<rec2[2] && rec1[2]>rec2[0] && rec1[1]<rec2[3] && rec1[3]>rec2[1]) {
+         return true;
+     }
+     return false;
+}
