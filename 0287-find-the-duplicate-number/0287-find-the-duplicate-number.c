0554e4401fff423cde5d817b40daabd3208398d2
Vibhor Shukla <2k24.cs1q.2414050@gmail.com>
2026-09-23 22:29:42 +0530

    Time: 11 ms (13.66%), Space: 14.9 MB (82.19%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.c
@@ -0,0 +1,17 @@
+int findDuplicate(int* nums, int numsSize) {
+    int s= nums[0];
+    int f= nums[0];
+
+    do {
+        s= nums[s];
+        f= nums[nums[f]];
+    } while (s != f);
+
+    s = nums[0];
+    while (s != f) {
+        s= nums[s];
+        f = nums[f];
+    }
+
+    return s;
+}
