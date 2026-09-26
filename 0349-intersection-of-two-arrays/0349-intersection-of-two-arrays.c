6a82fbfb4e5eb3a88b12273352149ab20068bd02
Vibhor Shukla <2k24.cs1q.2414050@gmail.com>
2026-09-26 21:42:21 +0530

    Time: 3 ms (40.47%), Space: 10.6 MB (65.54%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.c
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.c
@@ -12,23 +12,15 @@ int* intersection(int* nums1, int nums1Size,
                   int* returnSize) {
 
     qsort(nums2, nums2Size, sizeof(int), compare);
-
     int maxSize = nums1Size < nums2Size ? nums1Size : nums2Size;
-
     int* result = (int*)malloc(sizeof(int) * maxSize);
-
     *returnSize = 0;
-
     for (int i = 0; i < nums1Size; i++) {
-
         int low = 0;
         int high = nums2Size - 1;
         int foundInNums2 = 0;
-
         while (low <= high) {
-
             int mid = low + (high - low) / 2;
-
             if (nums1[i] == nums2[mid]) {
                 foundInNums2 = 1;
                 break;
@@ -40,24 +32,19 @@ int* intersection(int* nums1, int nums1Size,
                 low = mid + 1;
             }
         }
-
         if (foundInNums2) {
-
             int alreadyExists = 0;
-
             for (int k = 0; k < *returnSize; k++) {
                 if (result[k] == nums1[i]) {
                     alreadyExists = 1;
                     break;
                 }
             }
-
             if (!alreadyExists) {
                 result[*returnSize] = nums1[i];
                 (*returnSize)++;
             }
         }
     }
-
     return result;
 }
